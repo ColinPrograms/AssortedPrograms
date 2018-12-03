9573a82a95819c50e90894a75107c1328572c330
ColinPrograms <w6c0j@unb.ca>
2018-12-03 13:18:20 -0400

    completed Stockbroker Problem

---
+++ KattisProblems/java/3-4/DaydreamingStockbroker.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class DaydreamingStockbroker{
+
+	static Scanner sc = new Scanner(System.in);
+	static long money = 100;
+	static long days = sc.nextLong();
+	static long currentPrice = sc.nextLong();
+	static long next;
+	static long shares, count;
+
+	public static void main(String[] args){
+		boolean tradeIndicator = true; // buy == true, sell == false
+		shares = 0;
+		count = 1;
+		if(sc.hasNext()){
+			next = sc.nextLong();
+		}
+
+		while(count < days) {
+			if(tradeIndicator){
+				findBuyValue();
+				if(count < days){
+					buy();
+				}
+				tradeIndicator = false;
+			}else{
+				findSellValue();
+				sell();
+				tradeIndicator = true;
+			}
+		}
+		System.out.println(money);
+	}
+
+	public static void findSellValue(){
+		while(currentPrice <= next && days > count){
+			findNextValue();
+		}
+	}
+
+	public static void findBuyValue(){
+		while((currentPrice >= next || currentPrice > money) && days > count){
+			findNextValue();
+		}
+
+	}
+
+	public static void findNextValue(){
+		currentPrice = next;
+		if(count < days-1){
+			next = sc.nextInt();
+		}
+		count++;	
+	}
+
+	public static void buy(){
+		shares = Math.min(money/currentPrice, 100000);
+		money -= currentPrice*shares;	
+	}
+
+	public static void sell(){
+		money += currentPrice*shares;
+		shares = 0;
+	}
+}
